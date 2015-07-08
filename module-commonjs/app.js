1813c7714ae6f5924267a734cbd52ac0aef7e900
candyrobot <hideaki.tanaka@bizreach.co.jp>
2015-07-08 16:40:51 +0900

    minor change

--- module-commonjs/app.js
+++ module-commonjs/app.js
@@ -6,7 +6,7 @@ console.log(string.nl2br('some texts'));
 
 
 
-////////////////////////// or /////////////////////////
+/// or ///////////////////////////////////////////////
 var utils = require('./utils');
 
 console.log(utils.string.nl2br('some texts2'));
